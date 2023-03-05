4d3e673bf598e82d6a3e3b9849882a4a2992f7e5
Irina Zorina <irenfeliz@gmail.com>
2023-03-05 20:45:20 +0100

    new pazzle

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-text = input()
-count = 0
-while text not in ('стоп', 'хватит', 'достаточно'):
-    text = input()
-    count += 1
+# Дано натуральное число n.
+# Напишите программу, которая печатает численный треугольник с высотой равной n, в соответствии с примером:
+#
+# 1
+# 121
+# 12321
+# 1234321
+# 123454321
+# ...
 
-print(count)
+n = int(input())
+
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, end='')
+        if j == i:
+            for j in range(i - 1, 0, -1):
+                print(j, end='')
+
+    print()
