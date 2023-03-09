51c0c1758311e683887a1b441b5f7711badae95c
Irina Zorina <irenfeliz@gmail.com>
2023-03-09 20:51:25 +0100

    new pazzle

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-# Решите уравнение в натуральных числах 28n+30k+31m=365.
-#
-# Примечание. Используйте вложенный цикл for.
-# В первую очередь запишите решение с наименьшим значением n.
+n = int(input())
+digit = ''
 
-for n in range(1, 14):
-    for k in range(1, 13):
-        for m in range(1, 12):
-            if 28 * n + 30 * k + 31 * m == 365:
-                print('n =', n, 'k =', k, 'm =', m)
+while n > 0:
+    digit = str(n % 2) + digit
+    n //= 2
+print(digit)
+
+# for i in range(len(digit) - 1, -1, -1):
+#     print(digit[i], end='')
