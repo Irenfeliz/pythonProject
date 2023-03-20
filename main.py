c2137b735c9a96ff8f99e9e44327763ee64344fb
Irina Zorina <irenfeliz@gmail.com>
2023-03-20 18:27:35 +0100

    new pazzle

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
-# Решите уравнение в натуральных числах 28n+30k+31m=365.
+# import math
 #
-# Примечание. Используйте вложенный цикл for.
-# В первую очередь запишите решение с наименьшим значением n.
+# n = int(input())
+# print(math.ceil(math.log(n, 2)))
 
-for n in range(1, 14):
-    for k in range(1, 13):
-        for m in range(1, 12):
-            if 28 * n + 30 * k + 31 * m == 365:
-                print('n =', n, 'k =', k, 'm =', m)
+import random
+
+
+def is_valid(s):
+    if s.isdigit():
+        return True
+    else:
+        return False
+
+
+def game():
+    print('Добро пожаловать в числовую угадайку')
+    print('Угадываем число от 1 до ...?')
+    num = int(input())
+    r = random.randint(1, num)
+    print('Введите число от 1 до', num)
+    s = input()
+    flag = False
+    count = 0
+    while flag == False:
+        count += 1
+        while not is_valid(s):
+            print('А может быть все-таки введем целое число от 1 до', num, '?')
+            s = input()
+        n = int(s)
+        if n < r:
+            print('Ваше число меньше загаданного, попробуйте еще разок')
+            s = input()
+        if n > r:
+            print('Ваше число больше загаданного, попробуйте еще разок')
+            s = input()
+        if n == r:
+            print('Вы угадали, поздравляем!')
+            print('Вы отгадали с ', count, 'попыток!')
+            print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
+            flag = True
+
+
+game()
